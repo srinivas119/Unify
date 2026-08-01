fec6bf74633d927187049a85e07471024cc64ae1
srinivas119 <168756639+srinivas119@users.noreply.github.com>
2026-08-01 23:40:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
+import Profile from "./pages/profile";
 import PlatformConnection from "./pages/PlatformConnection";
 import VerifyEmail from "./pages/VerifyEmail";
 
