dd019af699426de586e6f5f3b86e9bf1316e797d
srinivas119 <168756639+srinivas119@users.noreply.github.com>
2026-08-01 23:58:21 +0530

    Configure CORS for specific origin

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://unify-pink.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api/dashboard", dashboardRoutes);
